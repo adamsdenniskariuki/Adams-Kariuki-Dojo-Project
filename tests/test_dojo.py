8946f4aadc34e25538ebed608b0248f67eadd074
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-06-10 22:33:51 +0300

    fixed tests errors

--- tests/test_dojo.py
+++ tests/test_dojo.py
@@ -10,22 +10,22 @@ class TestDojoClass(unittest.TestCase):
 	#test create office with errors
 	def test_dojo_create_office_with_errors(self):
 		dojo_input_error = self.dojo_instance.dojo_create_office(" &^*&^*^ ", "&(&(&(&(&")
-		self.assertEqual(dojo_input_error, "Use alphabet (a-z) characters for the room name and room type")
+		self.assertEqual(dojo_input_error, "Use alphabet a-z only for the room name and room type")
 
 	#test create office with errors
 	def test_dojo_create_livingspace_with_errors(self):
 		dojo_input_error = self.dojo_instance.dojo_create_livingspace(" &^*&^*^ ", "&(&(&(&(&")
-		self.assertEqual(dojo_input_error, "Use alphabet (a-z) characters for the room name and room type")
+		self.assertEqual(dojo_input_error, "Use alphabet a-z only for the room name and room type")
 
 	#test add fellow with errors
 	def test_dojo_add_fellow_with_errors(self):
 		dojo_input_error = self.dojo_instance.dojo_add_fellow(" &^*&^*^ ", "&(&(&(&(&", "UY^(Y**")
-		self.assertEqual(dojo_input_error, "Use alphabet (a-z) characters for the person name, type and wants accomodation")
+		self.assertEqual(dojo_input_error, "Use a-z only for the person name, type and wants accomodation")
 
 	#test add staff with errors
 	def test_dojo_add_staff_with_errors(self):
 		dojo_input_error = self.dojo_instance.dojo_add_staff(" &^*&^*^ ", "&(&(&(&(&", "UY^(Y**")
-		self.assertEqual(dojo_input_error, "Use alphabet (a-z) characters for the person name, type and wants accomodation")
+		self.assertEqual(dojo_input_error, "Use a-z only for the person name, type and wants accomodation")
 
 	#test create file with errors
 	def test_dojo_create_file_with_errors(self):
