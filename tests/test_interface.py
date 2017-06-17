a80f0631b9df276442cea81616d125b373953224
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-06-17 17:11:41 +0300

    fixed interface test

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -5,36 +5,36 @@ from interface.interface import Interface
 class TestDojoClass(unittest.TestCase):
 
 	def setUp(self):
-		self.interface_instance = Interface()
+		self.interface = Interface()
 
 	#test create office with errors
 	def test_create_office_with_errors(self):
-		input_error = self.interface_instance.create_office([" &^*&4^*^ "], "&(&(&(&(&")
+		input_error = self.interface.create_office([" &^*&4^*^ "], "&(&(&(&(&")
 		self.assertEqual(input_error, "Use a-z only for the room name and room type")
 
 	#test create office with errors
 	def test_create_livingspace_with_errors(self):
-		input_error = self.interface_instance.create_livingspace([" &^*&^54*^ "], "&(&(&(&(&")
+		input_error = self.interface.create_livingspace([" &^*&^54*^ "], "&(&(&(&(&")
 		self.assertEqual(input_error, "Use a-z only for the room name and room type")
 
 	#test add fellow with errors
 	def test_add_fellow_with_errors(self):
-		input_error = self.interface_instance.add_fellow(" &^*&^21*^ ", "Y")
+		input_error = self.interface.add_fellow(" &^*&^21*^ ", "Y")
 		self.assertEqual(input_error, "Use a-z only for the person name, type and wants accomodation")
 
 	#test add staff with errors
 	def test_add_staff_with_errors(self):
-		input_error = self.interface_instance.add_staff(" &^*&^54*^ ")
+		input_error = self.interface.add_staff(" &^*&^54*^ ")
 		self.assertEqual(input_error, "Use a-z only for the person name, type and wants accomodation")
 
 	#test create file with errors
 	def test_create_file_with_errors(self):
-		input_error = self.interface_instance.create_file("&(&(&(&(&", {})
+		input_error = self.interface.create_file("&(&(&(&(&", {})
 		self.assertEqual(input_error, "Use a-z only for the file name")
 
 	#test print room with errors
 	def test_print_room_with_errors(self):
-		input_error = self.interface_instance.print_room("myroom")
+		input_error = self.interface.print_room("myroom")
 		self.assertEqual(input_error, "Room myroom does not exist.")
 
 
