c05d98e13e95284d546eba75499dc8f70c792e1b
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-06-17 16:26:02 +0300

    fixed testing issues

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -19,17 +19,17 @@ class TestDojoClass(unittest.TestCase):
 
 	#test add fellow with errors
 	def test_add_fellow_with_errors(self):
-		input_error = self.interface_instance.add_fellow(" &^*&^21*^ ", "&(&(&(&(&", "Y")
+		input_error = self.interface_instance.add_fellow(" &^*&^21*^ ", "Y")
 		self.assertEqual(input_error, "Use a-z only for the person name, type and wants accomodation")
 
 	#test add staff with errors
 	def test_add_staff_with_errors(self):
-		input_error = self.interface_instance.add_staff(" &^*&^54*^ ", "&(&(&(&(&", "N")
+		input_error = self.interface_instance.add_staff(" &^*&^54*^ ")
 		self.assertEqual(input_error, "Use a-z only for the person name, type and wants accomodation")
 
 	#test create file with errors
 	def test_create_file_with_errors(self):
-		input_error = self.interface_instance.create_file("&(&(&(&(&")
+		input_error = self.interface_instance.create_file("&(&(&(&(&", {})
 		self.assertEqual(input_error, "Use a-z only for the file name")
 
 	#test print room with errors
